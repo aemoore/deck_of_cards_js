2ab935a88a4d6fbc128dc63cc563c6a0df5e3e24
Austin Moore <aemoore10@gmail.com>
2020-08-20 17:08:11 -0400

    sample run in index.js

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ import { default as Deck } from "./deck.js";
 
 var cards = new Deck();
 
+console.log("Example run of Deck of Cards.\nNew deck created:");
 console.log(cards.deck);
+console.log("shuffling deck...");
 cards.shuffle();
-console.log(cards.deck.length);
+console.log("done. Shuffled deck is:");
 console.log(cards.deck);
+console.log("dealing one card...");
 var card = cards.dealOneCard();
+console.log("card dealt:");
 console.log(card);
